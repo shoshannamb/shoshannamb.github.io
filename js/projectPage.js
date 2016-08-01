fa315e5a3e5c12feedbeb19d46a118afe0501ee8
Shoshanna Barnett <shoshannamb@gmail.com>
2016-08-01 09:06:54 -0400

    completed JS for desktop view

--- js/projectPage.js
+++ js/projectPage.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
-  var projectTextStartingBackground = $('#projects-text').css('background-color');
+  var projectTextStartingBackground = $('#project-text-box').css('background-color');
+  $('#im-text').hide();
+  $('#dvd-text').hide();
+  $('#unit-text').hide();
+  $('#console-text').hide();
+  var textId;
 
   $('.project-link').mouseover(function() {
     var color = $(this).css('background-color');
-    $('#projects-text').css('background-color',color);
+    var id = $(this).attr('id');
+    var idStem = id.substr(0, id.length - 4);
+    textId = "#" + idStem + "text";
+    $('#project-text-box').css('background-color',color);
+    $(textId).show();
+    $('#projects-text').hide();
   });
 
   $('.project-link').mouseleave(function() {
-    $('#projects-text').css('background-color',projectTextStartingBackground);
+    $('#project-text-box').css('background-color',projectTextStartingBackground);
+    $(textId).hide();
+    $('#projects-text').show();
   })
 });
